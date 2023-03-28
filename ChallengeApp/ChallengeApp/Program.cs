40ed5bc761fd8843bc09168b253af1cdee2f07b6
krman1 <mar.aniol@interia.pl>
2023-03-28 20:17:23 +0200

    zadaie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,15 +2,15 @@
 char sex = 'M';
 int age = 40;
 
-if(sex=='W' & age<30)
+if(sex=='W' && age<30)
     {
     Console.WriteLine("Kobieta poniżej 30 lat"); 
     }   
-else if(name == "Ewa" & age == 33) 
+else if(name == "Ewa" && age == 33) 
     {
     Console.WriteLine("Ewa lat 33");
     }
-else if( age<18 & sex=='M')
+else if( age<18 && sex=='M')
     {
     Console.WriteLine("Niepełnoletni mężczyzna");
     }
