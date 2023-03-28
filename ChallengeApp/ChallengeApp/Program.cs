2be47e0fca8687a270feb7bfd847b0cc3e4d25de
krman1 <mar.aniol@interia.pl>
2023-03-28 19:01:00 +0200

    Wpisanie imienia do aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Marcin !");
+﻿string name = "Marcin";
+char sex = 'M';
+int age = 40;
+
+if(sex=='W' & age<30)
+    {
+    Console.WriteLine("Kobieta poniżej 30 lat"); 
+    }   
+else if(name == "Ewa" & age == 33) 
+    {
+    Console.WriteLine("Ewa lat 33");
+    }
+else if( age<18 & sex=='M')
+    {
+    Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+else
+    {
+    Console.WriteLine("Żaden warunek nie jest spełniony");
+    }
