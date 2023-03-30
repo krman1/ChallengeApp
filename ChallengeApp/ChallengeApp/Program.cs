f1120ccf8acb53fb2bad3aa2d3218f72e8e92348
krman1 <mar.aniol@interia.pl>
2023-03-31 00:28:00 +0200

    zadanie dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,40 @@
-﻿int number = 1257857774;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using ChallengeApp;
+using System;
 
-Console.WriteLine("Wyniki dla liczby " + number);
-Console.WriteLine("==================================================");
-for (int k = 0; k < 10; k++)
-        {
-        int numberDigits = 0;
-        for (int i = 0; i < letters.Length; i++)
-            {
-             if (letters[i].ToString() == k.ToString())
-                {
-             numberDigits ++;
-                }
-             }
-        Console.WriteLine(k + " => " + numberDigits);
-        }
+List<Employee> employers = new List<Employee>();
+
+Employee employee1 = new Employee("Adam", "Nowak", 34);
+Employee employee2 = new Employee("Ewa", "Kowalska", 31);
+Employee employee3 = new Employee("Jan", "Grabowski", 25);
+employee1.AddScore(50);
+employee1.AddScore(1);
+employee1.AddScore(1);
+employee1.AddScore(1);
+employee1.AddScore(1);
+employee2.AddScore(2);
+employee2.AddScore(2);
+employee2.AddScore(2);
+employee2.AddScore(2);
+employee2.AddScore(2);
+employee3.AddScore(3);
+employee3.AddScore(3);
+employee3.AddScore(3);
+employee3.AddScore(3);
+employee3.AddScore(3);
+
+employers.Add(employee1);
+employers.Add(employee2);
+employers.Add(employee3);
+
+int maxResult = 0;
+Employee employeeWithMaxresult = null;
+foreach (var employee in employers)
+{
+    if(employee.Result >  maxResult)
+    { 
+       maxResult = employee.Result; 
+       employeeWithMaxresult = employee;
+    }
+}
+Console.WriteLine(employeeWithMaxresult.Name +" " + employeeWithMaxresult.Surname + " "+ employeeWithMaxresult.Age + " " + employeeWithMaxresult.Result);
+
