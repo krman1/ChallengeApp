1387f61821fb290d0c6c32d8f4039b1480f98381
krman1 <mar.aniol@interia.pl>
2023-04-03 18:53:52 +0200

    zadanie dzień 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,66 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+namespace ChallengeApp.Tests
+{
+    internal class TypeTests
+    {
+        [Test]
+        public void ComparisonReferenceTypes()
+        {
+            // arrange
+            var employee1 = CreateEmployee("Marcin");
+            var employee2 = CreateEmployee("Tomek");
+
+            // act
+           
+
+            // assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+        [Test]
+        public void ComparisonValueTypesInt()
+        {
+            // arrange
+            int number1 = 2;
+            int number2 = 2;
+
+            // act
+            
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void ComparisonValueTypesFloat()
+        {
+            // arrange
+            float number1 = 2.3f;
+            float number2 = 2.3f;
+
+            // act
+            
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void ComparisonStrings()
+        {
+            // arrange
+            String text1 = "kurs programowania";
+            String text2 = "kurs programowania";
+
+            // act
+
+
+            // assert
+            Assert.AreEqual(text1, text2);
+        }
+        private Employee CreateEmployee(string name)
+        {
+            return new Employee(name);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -10,6 +10,10 @@
             this.Surname = surname;
             this.Age = age;
         }
+        public Employee(string name)
+        {
+            this.Name = name;
+        }
 
         public string Name
         {
